a52ebbe8c7974d4c88195aaa3cec52034e02e294
chenzhao <chenzhaoplus@163.com>
2021-06-15 18:24:09 +0800

    模块引用

--- helloworld/module_test.py
+++ helloworld/module_test.py
@@ -1,5 +1,5 @@
-# from testmysql import hello_mysql as hm
-import testmysql.hello_mysql as hm
+# from test_mysql import hello_mysql as hm
+import test_mysql.hello_mysql as hm
 
 if __name__ == '__main__':
     hm.select()
