1113f58e1c6db600c7e6eaa0b9b654596f18e9a2
chenzhao <chenzhaoplus@163.com>
2021-07-22 10:41:20 +0800

    flask

--- test_flask/test_flask.py
+++ test_flask/test_flask.py
@@ -2,19 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-"""
-shell:
-    export FLASK_APP=test_flask
-    flask run
-cmd:
-    set FLASK_APP=test_flask
-    flask run
-powershell:
-    $env:FLASK_APP = "test_flask"
-    flask run
-"""
-
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    """
+    如果没有main方法，则需要执行下面指令启动
+    shell:
+        export FLASK_APP=test_flask
+        flask run
+    cmd:
+        set FLASK_APP=test_flask
+        flask run
+    powershell:
+        $env:FLASK_APP = "test_flask"
+        flask run
+    """
+    app.run()
